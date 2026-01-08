3123573bb568857aebcd718f617987184d66fc5b
skril349 <tvives.cabaleiro@gmail.com>
2026-01-08 12:40:30 +0100

    preparing db file

---
+++ app/core/db.py
@@ -0,0 +1,15 @@
+from typing import Iterator
+from app.core.config import settings
+from sqlmodel import SQLModel, Session, create_engine
+
+engine = create_engine(settings.DATABASE_URL, echo=False, connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {})
+
+
+def init_db() -> None:
+    SQLModel.metadata.create_all(engine) # dev
+    
+def get_session() -> Iterator[Session]:
+    with Session(engine) as session:
+        yield session
+
+    

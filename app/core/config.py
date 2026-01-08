97902159a05fa3530b02269c078042a62b2daaf0
skril349 <tvives.cabaleiro@gmail.com>
2026-01-08 11:59:39 +0100

    adding config and settings

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = Field(..., env="DATABASE_URL")
+    JWT_SECRET: str = Field(..., env="JWT_SECRET")
+    JWT_ALG: str = Field(default="HS256", env="JWT_ALG")
+    JWT_EXPIRES_MIN: int = Field(default=60*24, env="JWT_EXPIRES_MIN")
+    PROJECT_NAME: str = Field(default="FastAPI Devinote", env="PROJECT_NAME")
+    
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+    
+settings = Settings()
+

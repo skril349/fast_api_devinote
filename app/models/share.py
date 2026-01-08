96cca0632dd9899801076de42d35b02e18f8e483
skril349 <tvives.cabaleiro@gmail.com>
2026-01-08 13:18:32 +0100

    share model

---
+++ app/models/share.py
@@ -0,0 +1,29 @@
+
+
+from enum import Enum
+
+from sqlalchemy import UniqueConstraint
+from sqlmodel import SQLModel, Field
+
+
+class ShareRole(str,Enum):
+    READ = "read"
+    EDIT = "edit"
+
+class NoteShare(SQLModel, table=True):
+    
+    __tablename__ = "note_share"
+    __table_args__ = (UniqueConstraint("note_id", "user_id", name="uix_note_shareduser"))
+    id: int = Field(default=None, primary_key=True)
+    note_id: int = Field(foreign_key="note.id", index=True)
+    user_id: int = Field(foreign_key="user.id", index=True)
+    role: ShareRole = Field(default=ShareRole.READ)
+    
+class LabelShare(SQLModel, table=True):
+    
+    __tablename__ = "label_share"
+    __table_args__ = (UniqueConstraint("label_id", "user_id", name="uix_label_shareduser"))
+    id: int = Field(default=None, primary_key=True)
+    label_id: int = Field(foreign_key="label.id", index=True)
+    user_id: int = Field(foreign_key="user.id", index=True)
+    role: ShareRole = Field(default=ShareRole.READ)
